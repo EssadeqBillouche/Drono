6df00a2ea82578aa94e20b44991da30a605232dc
MrNobodyCode <essadeq.billouche@gmail.com>
2025-03-28 11:32:19 +0000

    init routes for views

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/profile', function () { return view('Index'); })->name('home');
+
+Route::get('/cart', function () {return view('cart');})->name('cart');
+
+Route::get('/catalog', function () {return view('catalog');})->name('catalog');
+
+
+
+Route::get('/carddd', function () {return view('Index');})->name('checkout.shipping');
+
+
+Route::get('/checkout', function () {return view('checkout');})->name('checkout');
+
+
+Route::get('/login', function () { return view('Auth.Login'); })->name('login');
+
+Route::get('/register', function () {
+    return view('Auth.register');
+})->name('register');
+
+Route::get('/ResetPassword', function () {
+    return view('Auth.ResetPassword');
+})->name('ResetPassword');
