9d2df7796033ba4fcedd30261546125d1a831ad9
MrNobodyCode <essadeq.billouche@gmail.com>
2025-04-03 16:44:37 +0000

    user Entity

---
+++ app/Domain/Auth/Entities/User.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Domain\Auth\Entities;
+
+use App\Domain\Auth\ValueObjects\Email;
+use App\Domain\Auth\ValueObjects\Role;
+use App\Domain\Auth\ValueObjects\UserId;
+
+class User
+{
+    private UserId $id;
+    private Email $email;
+    private string $password;
+    private Role $role;
+    private string $name;
+
+    public function __construct(UserId $id, Email $email, string $password, Role $role, string $name)
+    {
+        $this->id = $id;
+        $this->email = $email;
+        $this->password = $password;
+        $this->role = $role;
+        $this->name = $name;
+    }
+
+    public function getId(): UserId
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): Email
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function getRole(): Role
+    {
+        return $this->role;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+
+}
