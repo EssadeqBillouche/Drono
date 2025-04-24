421e640cf7aed14342f7cea8b47c818ea3cb7302
MrNobodyCode <essadeq.billouche@gmail.com>
2025-04-24 15:00:41 +0100

    Add validation and DTO transformation in UpdateProductRequest
    Implemented validation rules to ensure request data integrity and added a `toDTO` method to transform validated data into an `UpdateProductDTO` object. The changes also include handling for uploading images and generating slugs from product names.

--- app/Presentation/Http/Requests/Product/UpdateProductRequest.php
+++ app/Presentation/Http/Requests/Product/UpdateProductRequest.php
@@ -2,7 +2,44 @@
 
 namespace App\Presentation\Http\Requests\Product;
 
-class UpdateProductRequest
-{
+use App\Application\Product\DTOs\UpdateProductDTO;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
+class UpdateProductRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules(): array
+    {
+        return [
+            'sellerId' => 'sometimes|integer',
+            'categoryId' => 'sometimes|integer',
+            'name' => 'sometimes|string|max:255',
+            'description' => 'sometimes|string',
+            'price' => 'sometimes|numeric|min:0',
+            'stock' => 'sometimes|integer|min:0',
+            'isActive' => 'sometimes|boolean',
+            'images' => 'sometimes|image',
+        ];
+    }
+
+    public function toDTO(): UpdateProductDTO
+    {
+        $data = $this->validated();
+
+        if ($this->hasFile('images')) {
+            $data['images'] = Storage::putFile('product_images', $this->file('images'));
+        }
+
+        if (isset($data['name'])) {
+            $data['slug'] = Str::slug($data['name']);
+        }
+
+        return new UpdateProductDTO(...$data);
+    }
 }
