ab7cafbd36653c6bddaa5a4a4df5847d59b78eaa
MrNobodyCode <essadeq.billouche@gmail.com>
2025-04-24 15:00:28 +0100

    Update AddProductRequest validation and DTO generation logic
    Refactored field names to use snake_case for consistency and replaced 'images' with 'image' validation. Updated image upload logic to use the correct path and set sellerId automatically from the authenticated user.

--- app/Presentation/Http/Requests/Product/AddProductRequest.php
+++ app/Presentation/Http/Requests/Product/AddProductRequest.php
@@ -17,27 +17,24 @@ class AddProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'sellerId' => 'required|integer',
-            'categoryId' => 'required|integer',
+            'category_id' => 'required|integer',
             'name' => 'required|string|max:255',
             'slug' => 'nullable|string|max:255',
             'description' => 'string',
             'price' => 'required|numeric|min:0',
             'stock' => 'required|integer|min:0',
             'isActive' => 'required|boolean',
-            'images' => 'required|image', // Assuming 'images' is now a file upload
+            'image' => 'required|image', // Assuming 'images' is now a file upload
         ];
     }
 
 
     public function toDTO(): AddProductDTO
     {
-        // Upload the image and get the path
-        $imagePath = Storage::putFile('product_images', $this->file('images'));
-
+        $imagePath = Storage::putFile('products', $this->file('images'));
 
         return new AddProductDTO(
-            sellerId: $this->validated('sellerId'),
+            sellerId: auth()->user()->id,
             categoryId: $this->validated('categoryId'),
             name: $this->validated('name'),
             slug: Str::slug($this->validated('name')),
