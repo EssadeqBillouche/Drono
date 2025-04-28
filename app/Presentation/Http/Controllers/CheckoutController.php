8ff5dc2012f49391d5f74c8cfb88cc30eb6f208f
MrNobodyCode <essadeq.billouche@gmail.com>
2025-04-28 09:26:59 +0100

    Add CheckoutController with basic structure
    Introduces a new CheckoutController with placeholder methods `create` and `index`. This lays the groundwork for handling checkout-related functionality in the application.

---
+++ app/Presentation/Http/Controllers/CheckoutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Presentation\Http\Controllers;
+
+class CheckoutController extends Controller
+{
+    public function __construct(){}
+    public function create(){}
+    public function index(){
+    }
+
+}
