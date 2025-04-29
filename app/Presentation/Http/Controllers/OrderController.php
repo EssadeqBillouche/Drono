713832db00c99ae5a74f01fa8068b2ad8680cc0f
MrNobodyCode <essadeq.billouche@gmail.com>
2025-04-29 16:44:41 +0100

    Add basic structure for OrderController with stub methods
    Introduced `create`, `show`, and `index` methods in `OrderController`. Added a new `CreateOrderRequest` to handle request validation for the `create` method, currently dumping all request data for debugging purposes. Extended `OrderController` to

--- app/Presentation/Http/Controllers/OrderController.php
+++ app/Presentation/Http/Controllers/OrderController.php
@@ -2,7 +2,20 @@
 
 namespace App\Presentation\Http\Controllers;
 
-class OrderController
+
+use App\Presentation\Http\Requests\Order\CreateOrderRequest;
+
+class OrderController extends Controller
 {
+    public function __construct()
+    {
+    }
+    public function create( CreateOrderRequest $request){
+
+        dd($request->all());
+
+    }
+    public function show(){}
+    public function index(){}
 
 }
