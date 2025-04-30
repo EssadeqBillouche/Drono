b59bd2478d54aa584056658fb2f373f4c4b5b3df
MrNobodyCode <essadeq.billouche@gmail.com>
2025-04-30 09:14:00 +0100

    Add Order and OrderServiceProvider to providers list
    This commit ensures the inclusion of Order and OrderServiceProvider in the providers array. This change is necessary to register the associated services and enhance the application's functionality.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Order::class,
+    App\Providers\OrderServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
 ];
