b9e273ce74a24fa5324bb91c11a96b77db05b97e
MrNobodyCode <essadeq.billouche@gmail.com>
2025-04-30 09:15:16 +0100

    Add properties and methods to AddOrderDTO
    This update introduces new properties to the AddOrderDTO class, including items, shipping coordinates, client ID, and optional notes. Corresponding getter methods and a constructor were added to improve data handling and accessibility for orders.

--- app/Application/Orders/DTOs/AddOrderDTO.php
+++ app/Application/Orders/DTOs/AddOrderDTO.php
@@ -4,5 +4,51 @@ namespace App\Application\Orders\DTOs;
 
 class AddOrderDTO
 {
+    private array $items;
+    private string $shippingLatitude;
+    private string $shippingLongitude;
+
+    private int $clientId;
+    private ?string $notes;
+
+    public function __construct(
+        array $items,
+        string $shippingLatitude,
+        string $shippingLongitude,
+        int $clientId,
+        ?string $notes = null
+    ) {
+        $this->items = $items;
+        $this->shippingLongitude = $shippingLongitude;
+        $this->shippingLatitude = $shippingLatitude;
+        $this->clientId = $clientId;
+        $this->notes = $notes;
+    }
+
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    public function getShippingAddress(): string
+    {
+        return $this->shippingAddress;
+    }
+
+    public function getBillingAddress(): string
+    {
+        return $this->billingAddress;
+    }
+
+    public function getClientId(): int
+    {
+        return $this->clientId;
+    }
+
+    public function getNotes(): ?string
+    {
+        return $this->notes;
+    }
+
 
 }
