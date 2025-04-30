21b2bf64b840c4fadde20b88bc1bd8d27c6fa314
MrNobodyCode <essadeq.billouche@gmail.com>
2025-04-30 09:14:44 +0100

    Add `store` and `update` methods to OrderDAO
    Implemented `store` for saving orders and `update` for modifying them. This enhances the existing OrderDAO class to comply with

--- app/Infrastructure/Persistence/DAOs/OrderDAO.php
+++ app/Infrastructure/Persistence/DAOs/OrderDAO.php
@@ -2,9 +2,12 @@
 
 namespace App\Infrastructure\Persistence\DAOs;
 
-use App\Domain\Products\Repositories\OrderRepositoryInterface;
+use App\Application\Orders\DTOs\AddOrderDTO;
+use App\Domain\Order\Entities\Order;
+use App\Domain\Order\Repositories\OrderRepositoryInterface;
 
 class OrderDAO implements OrderRepositoryInterface
 {
-
+    public function store(Order $order){}
+    public function update(){}
 }
