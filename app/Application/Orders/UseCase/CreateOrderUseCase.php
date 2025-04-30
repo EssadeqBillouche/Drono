42830f6f99981cc79d2e0001ac0eccde9f4baf74
MrNobodyCode <essadeq.billouche@gmail.com>
2025-04-30 09:15:29 +0100

    Add dependencies and stub method to CreateOrderUseCase
    Introduce constructor dependency injection for OrderRepositoryInterface. Added a stub `execute` method with basic iteration over AddOrderDTO items for future implementation.

--- app/Application/Orders/UseCase/CreateOrderUseCase.php
+++ app/Application/Orders/UseCase/CreateOrderUseCase.php
@@ -2,7 +2,24 @@
 
 namespace App\Application\Orders\UseCase;
 
+use App\Application\Orders\DTOs\AddOrderDTO;
+use App\Domain\Order\Entities\Order;
+use App\Domain\Order\Entities\OrderItem;
+use App\Domain\Order\Repositories\OrderRepositoryInterface;
+use PhpYacc\Lalr\Item;
+
 class CreateOrderUseCase
 {
+    private $orderDAO;
+    public function __construct( OrderRepositoryInterface $orderDAO){
+        $this->orderDAO = $orderDAO;
+    }
 
+    public function execute(AddOrderDTO $addOrderDTO){
+
+            foreach ($addOrderDTO->getItems() as $item) {
+                dd($item['id']);
+            }
+
+    }
 }
